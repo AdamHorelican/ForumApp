1b2c350f38d04efce30ef43f385b5f57ef8b6aac
AdamHorelican <horelicanadam@gmail.com>
2026-09-08 14:34:41 +0200

    Git fix

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -2,5 +2,9 @@
 
 public class Comment
 {
-    
+    public int Id { get; set; }
+    public string Body { get; set; }
+    public string UsernameId { get; set; }
+    public HashSet<int> LikedCommentIds { get; set; } = new HashSet<int>();
+    public HashSet<int> DislikedCommentIds { get; set; } = new HashSet<int>();
 }
