5bd486a092acecf1241981b3f366205eb5055b80
AdamHorelican <horelicanadam@gmail.com>
2026-09-08 14:46:06 +0200

    CRUD for Post

---
+++ RepositaryContracts/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+
+namespace RepositaryContracts;
+
+public interface IPostRepository
+{
+    Task<Post> AddAsync(Post post);
+    Task UpdateAsync(Post post);
+    Task DeleteAsync(int id);
+    Task<Post> getByIdAsync(int id);
+    IQueryable<Post> getAll();
+}
