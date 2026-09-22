6dd7068d7e283b7165f3a909a8a4913a6170b7c0
AdamHorelican <horelicanadam@gmail.com>
2026-09-22 09:49:07 +0200

    Add fileRepository, changed refrenced so InMemoryRepository is no longer in use

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,13 +1,14 @@
 ﻿using Console.UI;
+using FileRepositories;
 using InMemoryRepositories;
 using RepositaryContracts;
 
 System.Console.WriteLine("Starting ....");
 
 
-IUserRepository userRepo = new UserInMemoryRepository();
-IPostRepository postRepo = new PostInMemoryRepository();
-ICommentRepository commentRepo = new CommentInMemoryRepository();
+IUserRepository userRepo = new UserFileRepository();
+IPostRepository postRepo = new PostFileRepository();
+ICommentRepository commentRepo = new CommentFileRepository();
 
 CliApp app = new CliApp(userRepo, postRepo, commentRepo);
 await app.StartAsync();
---
+++ FileRepositories/CommentFileRepository.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using RepositaryContracts;
+
+namespace FileRepositories;
+
+public class CommentFileRepository : ICommentRepository
+{
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Comment> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Comment> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+}
